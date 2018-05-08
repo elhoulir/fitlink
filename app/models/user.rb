3682ccc9b05ee218939fd53eae544b9ce4533fc6
Rashid Elhouli <rashidelhouli@gmail.com>
2018-05-08 12:11:30 +1000

    add relations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :profile 
+  has_many :booked_gym_sessions
+  has_many :gyms
+
+
 end
