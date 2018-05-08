3bb131e5e372a6c5fb92e3e6eb5178f7f779ef89
Rashid Elhouli <rashidelhouli@gmail.com>
2018-05-08 14:17:51 +1000

    add sign in and sign out links to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
-
-  resources :gyms do
-    
-    resources :gym_sessions 
   
+  resources :gyms do
+    resources :gym_sessions 
   end
   
   devise_for :users
+  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
