ff72c065471ccc23594a740f08c4763cb94d12f8
Rashid Elhouli <rashidelhouli@gmail.com>
2018-05-08 16:35:29 +1000

    add migration profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   end
   
   devise_for :users
-  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
