584c5a060990ce53c47f8797b32435b06a4323b1
Rashid Elhouli <rashidelhouli@gmail.com>
2018-05-08 14:37:06 +1000

    add profiles scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508015457) do
+ActiveRecord::Schema.define(version: 20180508043637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,18 +53,8 @@ ActiveRecord::Schema.define(version: 20180508015457) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "contact_number"
-    t.date "date_of_birth"
-    t.string "address"
-    t.string "drivers_license"
-    t.text "drivers_license_image_data"
-    t.text "photo_data"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -92,5 +82,4 @@ ActiveRecord::Schema.define(version: 20180508015457) do
   add_foreign_key "booked_gym_sessions", "users"
   add_foreign_key "gym_sessions", "users"
   add_foreign_key "gyms", "users"
-  add_foreign_key "profiles", "users"
 end
